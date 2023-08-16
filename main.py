276f9e9a381071b075aad055417029182877cac3
Roman Vetokhin <r.vetokhin@gmail.com>
2023-08-16 17:50:32 +0600

    add new bot

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
-import openai
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+
 import json
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 
 file = open('config.json', 'r')
 config = json.load(file)
 
-openai.api_key = config['openai']
-bot = Bot(config['token'])
-dp = Dispatcher(bot)
+# # openai.api_key = config['openai']
+# bot = Bot(config['token'])
+# dp = Dispatcher(bot)
 
-messages = [
-    {"role": "system", "content": "You are a sales consultant"},
-    {"role": "user", "content": "I am a client"},
-    {"role": "assistent", "content": "Greatings! ..."}]
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = config['token']
 
-print(messages)
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
 
-def update(messages, role, content):
-    messages.append({"role": role, "content": content})
-    return messages
 
-@dp.message_handler()
-async def send(message : types.Message):
-    bot_info = await message.bot.get_me()
-    print(message.text)
-    # if f'@{bot_info.username}' in message.text:
-    #     update(messages, "user", message.text)
-    #     response = openai.ChatCompletion.create(
-    #         model = "gpt-3.5-turbo",
-    #         message = messages
-    #     )
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {hbold(message.from_user)}!")
 
-    #     await messages.answer(response['choices'][0]['message']['content'])
 
-executor.start_polling(dp, skip_updates=True)
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot stopped!")
